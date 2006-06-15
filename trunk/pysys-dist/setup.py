ac1acaa826f28a0c7a5b883ef10a5414759ec58d
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2006-06-15 21:24:07 +0000

    Import sources

---
+++ trunk/pysys-dist/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PySys',
+      version='0.1.1',
+      description='Python System Test Framework',
+      author='Moray Grieve',
+      author_email='moraygrieve@users.sourceforge.net',
+      url="http://sourceforge.net/projects/pysys",
+      packages=['pysys', 'pysys.launcher',  'pysys.manual',
+                'pysys.process',  'pysys.process.plat-win32',
+                'pysys.process.plat-unix', 'pysys.utils', 'pysys.xml']
+     )
