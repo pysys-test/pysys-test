8f84bebb63eaacd09684a3df43e262a488420d62
Moray Grieve <moraygrieve@users.noreply.github.com>
2006-07-12 10:05:16 +0000

    updated for new test maker

--- pysys-examples/fibonacci/testcases/makeTest.py
+++ pysys-examples/fibonacci/testcases/makeTest.py
@@ -3,5 +3,6 @@ import os, sys
 from pysys.launcher.console import ConsoleMakeTestHelper
 
 if __name__ == "__main__":
-	printer = ConsoleMakeTestHelper(os.getcwd(), "from pysys.basetest import BaseTest", "BaseTest")
-	printer.parseArgs(sys.argv[1:])
+	maker = ConsoleMakeTestHelper(os.getcwd())
+	maker.parseArgs(sys.argv[1:])
+	maker.makeTest()
