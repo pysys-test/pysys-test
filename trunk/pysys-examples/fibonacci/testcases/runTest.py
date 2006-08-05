f3790bfba588bc4ac197c894caa2bde58dc70cc1
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2006-08-05 08:38:00 +0000

    interim update

--- trunk/pysys-examples/fibonacci/testcases/runTest.py
+++ trunk/pysys-examples/fibonacci/testcases/runTest.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python
 import sys
 from pysys.baserunner import BaseRunner
-from pysys.writer import ConsoleResultsWriter
 from pysys.launcher.console import ConsoleLaunchHelper
 
 if __name__ == "__main__":
 	launcher = ConsoleLaunchHelper()
 	launcher.parseArgs(sys.argv[1:])
-	launcher.runTests(BaseRunner, [ConsoleResultsWriter])
+	launcher.runTests(BaseRunner)
