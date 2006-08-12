0b9f48c187bef8446ff16c032fd6ec7d32a1e124
Moray Grieve <moraygrieve@users.noreply.github.com>
2006-08-12 08:01:01 +0000

    Adding post install script

--- pysys-dist/setup.py
+++ pysys-dist/setup.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
 import pysys, glob, sys, os
+import distutils.sysconfig
 from distutils.core import setup
 
-def get_site_packages_path():
-	if sys.platform.lower().startswith('win'):
-		return os.path.join("Lib", "site-packages")
-	else:
-		return os.path.join("lib", "python%s" % sys.version[:3], "site-packages")
-
+lib_dir = distutils.sysconfig.get_python_lib(plat_specific=1)
 
 docfiles = ['pysys-doc/epydoc.css', 'pysys-doc/epydoc.js']
 docfiles.extend(glob.glob('pysys-doc/*.html'))
+
 
 setup(name='PySys',
 		version=pysys.__version__,
@@ -21,5 +18,6 @@ setup(name='PySys',
 		url="http://sourceforge.net/projects/pysys",
 		packages=['pysys', 'pysys.launcher',  'pysys.manual', 'pysys.process', 'pysys.process.plat-win32', 
 		          'pysys.process.plat-unix', 'pysys.utils', 'pysys.writer', 'pysys.xml'],
-		data_files=[('%s/pysys-doc' % get_site_packages_path(), docfiles)]
+		data_files=[('%s/pysys-doc' % lib_dir, docfiles)],
+		scripts = ['pysys-dist/pysys_postinstall.py']
 		)
