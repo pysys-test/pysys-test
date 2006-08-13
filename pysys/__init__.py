ccaefc78c50809f608eeb3d9c576e6d95f508986
Moray Grieve <moraygrieve@users.noreply.github.com>
2006-08-13 20:36:29 +0000

    More doc comments

--- pysys/__init__.py
+++ pysys/__init__.py
@@ -1,5 +1,22 @@
 """PySys System Test Framework.
 
+PySys has been designed using OO techniques to provide a generic extensible framework for the organisation and execution of 
+system level testcases. It provides a clear abstraction of what a testcases is, how it is structured on disk, how it is executed 
+and validated, and how the outcome is reported for test auditing purposes. 
+
+Testcases are instances of a base test class (L{pysys.basetest.BaseTest}) which provides core functionality for cross platform 
+process management and monitoring; in this manner an application under test (AUT) can be started and manipulated directly 
+within the testcase. The base test class additionally provides a set of standard validation techniques based predominantly 
+on regular expression matching within text files (e.g. stdout, logfile of the AUT etc). Testcases are executed through a base 
+runner (L{pysys.baserunner.BaseRunner}) which provides the mechanism to control testcase flow and auditing. In both cases the 
+base test and runner classes have been designed to be extended for a particular AUT, e.g. to allow a higher level of abstraction 
+of AUT manipulation, tear up and tear down prior to executing a set of testcases etc. 
+
+PySys allows automated regression testcases to be built rapidly. Where an AUT cannot be tested in an automated fashion, testcases 
+can be written to make use of a manual test user interface application (L{pysys.manual.ui.ManualTester}) which allows the steps 
+required to execute the test to be presented to a tester in a clear and navigable manner. The tight integration of both manual and 
+automated testcases provides a single system for all test organisation requirements. 
+
 """
 
 import sys, logging
@@ -14,10 +31,10 @@ __author_email__ = "moraygrieve@users.sourceforge.net"
 __status__  = "alpha"
 """The status of this release."""
 
-__version__ = "0.1.7"
+__version__ = "0.2.0"
 """The version of this release."""
 
-__date__    = "11 August 2006"
+__date__    = "14 August 2006"
 """The date of this release."""
 
 __all__     = [ "constants",
