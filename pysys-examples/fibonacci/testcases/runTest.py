24fb40e581c4e1c93e845d700b560012ddaea795
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-02-06 10:10:30 +0000

    Add setup and cleanup for writers

--- pysys-examples/fibonacci/testcases/runTest.py
+++ pysys-examples/fibonacci/testcases/runTest.py
@@ -1,9 +1,14 @@
 #! /usr/bin/env python2.4
-import sys
+import sys, time
 from pysys.baserunner import BaseRunner
+from pysys.writer import LogFileResultsWriter
 from pysys.launcher.console import ConsoleLaunchHelper
 
 if __name__ == "__main__":
+	# create a logfile results writer
+	logwriter = LogFileResultsWriter("testsummary_%s.log" % time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time())))
+
+	# parse the args and run the tests
 	launcher = ConsoleLaunchHelper()
 	launcher.parseArgs(sys.argv[1:])
-	launcher.runTests(BaseRunner)
+	launcher.runTests(runner = BaseRunner, writers = [logwriter])
