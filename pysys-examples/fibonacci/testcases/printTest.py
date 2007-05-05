09a756a2b8b9bda819e4c5fef2d3861fd5e2db36
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-05-05 10:16:32 +0000

    Made the runTest modules explicitly create the runner and call it's start method

--- pysys-examples/fibonacci/testcases/printTest.py
+++ pysys-examples/fibonacci/testcases/printTest.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python2.4
-import sys
+import os, sys
 from pysys.launcher.console import ConsolePrintHelper
 
 if __name__ == "__main__":
-	printer = ConsolePrintHelper()
+	printer = ConsolePrintHelper(os.getcwd())
 	printer.parseArgs(sys.argv[1:])
 	printer.printTests()
