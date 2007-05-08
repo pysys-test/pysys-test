49f70d3d34117f5f5bbac101f20f073fac745cb3
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-05-08 07:59:23 +0000

    Doc updates

--- pysys/utils/filecopy.py
+++ pysys/utils/filecopy.py
@@ -21,34 +21,46 @@
 
 import sys
 
-def __copyfileobj(fsrc, fdst, length=16*1024):
-   while 1:
-      buf = fsrc.read(length)
-      if not buf:
-         break
-      fdst.write(buf)
+def copyfileobj(fsrc, fdst, length=16*1024):
+	"""Internal method to read bytes from a source file descriptor, and write to a destination file descriptor.
+	
+	@param fsrc: The source file descriptor
+	@param fdst: The destination file descriptor
+	@param length: The buffer length to read from the src and write to the destination
+	
+	"""
+	while 1:
+		buf = fsrc.read(length)
+		if not buf:
+			break
+		fdst.write(buf)
 
 
 def filecopy(src, dst):
-   fsrc = None
-   fdst = None
-   try:
-      fsrc = open(src, 'rb')
-      fdst = open(dst, 'wb')
-      __copyfileobj(fsrc, fdst)
-   finally:
-      if fdst:
-         fdst.close()
-      if fsrc:
-         fsrc.close()
+	"""Copy source file to a destination file.
+	
+	@param src: Full path to the source filename
+	@param dst: Full path the destination filename
+ 
+	"""
+	fsrc = None
+	fdst = None
+	try:
+		fsrc = open(src, 'rb')
+		fdst = open(dst, 'wb')
+		copyfileobj(fsrc, fdst)
+	finally:
+		if fdst:
+			fdst.close()
+		if fsrc:
+			fsrc.close()
 
 
 # entry point for running the script as an executable
 if __name__ == "__main__":
-   if len(sys.argv) < 2:
-      print "Usage: fileunzip <src> <dst>"
-      sys.exit()
-   else:
-      filecopy(sys.argv[1], sys.argv[2])
-      
+	if len(sys.argv) < 2:
+		print "Usage: fileunzip <src> <dst>"
+		sys.exit()
+	else:
+		filecopy(sys.argv[1], sys.argv[2])
 
