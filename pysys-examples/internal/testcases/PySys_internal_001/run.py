a0017748877e3211ceb70092e46bd8c6f71bf7b3
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-08-19 22:05:13 +0000

    Updated

--- pysys-examples/internal/testcases/PySys_internal_001/run.py
+++ pysys-examples/internal/testcases/PySys_internal_001/run.py
@@ -1,36 +1,20 @@
 from pysys.constants import *
 from pysys.basetest import BaseTest
-from pysys.process.helper import ProcessWrapper
+from pysys.constants import Project 
 
 class PySysTest(BaseTest):
 
 	def execute(self):
-		script = "%s/internal/utilities/scripts/counter.py" % self.project.root
+		os.environ["TEST_USER"] = "Felicity Kendal"
+		self.proj = Project(self.input)
 	
-		self.hprocess = self.startProcess(command=sys.executable,
-						  arguments = [script, "2", "-101"],
-						  environs = os.environ,
-						  workingDir = self.input,
-						  stdout = "%s/counter.out" % self.output,
-						  stderr = "%s/counter.err" % self.output,
-						  state=FOREGROUND)
-		
-		# do a couple of wait for files
-		self.waitForFile('counter.out', timeout=4)
-		self.waitForFile('counter.err', timeout=4)
-						  
-		# do a couple of wait for signals in the files
-		self.waitForSignal('counter.out', expr='Count is 1', timeout=4)
-		self.waitForSignal('counter.err', expr='Process id of test executable', timeout=4)	
-
-		
 	def validate(self):
-		# check the sdtout of the process
-		self.assertDiff('counter.out', 'ref_counter.out')
-		
-		# check the stderr of the process
-		self.assertGrep('counter.err', expr='Process id of test executable is %d' % self.hprocess.pid)
-		
-		# check the return status of the process
-		self.assertTrue(self.hprocess.exitStatus == -101)
-		
+		self.assertTrue(self.proj.env_user == "Felicity Kendal")
+		self.assertTrue(self.proj.env_user_prepend == "append-on-front-Felicity Kendal")
+		self.assertTrue(self.proj.env_user_append == "Felicity Kendal-append-on-back")
+		self.assertTrue(self.proj.env_default == "default value")
+		self.assertTrue(self.proj.env_default_none == "")
+		self.assertTrue(self.proj.user_firstname == "Simon")
+		self.assertTrue(self.proj.user_lastname == "Smith")
+		self.assertTrue(self.proj.user_title == "Professor")
+		self.assertTrue(self.proj.user_full == "Professor Simon Smith")
