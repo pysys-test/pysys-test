25f9642a984c0f4b0a8510c63ab9ecf6aadbe2e4
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-08-20 09:47:16 +0000

    Addition

--- pysys-examples/internal/testcases/PySys_internal_021/run.py
+++ pysys-examples/internal/testcases/PySys_internal_021/run.py
@@ -1,9 +1,37 @@
 from pysys.constants import *
 from pysys.basetest import BaseTest
+from pysys.process.helper import ProcessWrapper
 
 class PySysTest(BaseTest):
-	def execute(self):
-		pass
 
+	def execute(self):
+		script = "%s/internal/utilities/scripts/counter.py" % self.project.root
+	
+		self.hprocess = self.startProcess(command=sys.executable,
+						  arguments = [script, "10", "0"],
+						  environs = os.environ,
+						  workingDir = self.input,
+						  stdout = "%s/counter.out" % self.output,
+						  stderr = "%s/counter.err" % self.output,
+						  state=BACKGROUND)
+						  
+		# check the process status
+		self.initialstatus = self.hprocess.running())
+		
+		# wait for the process to complete (after 10 loops)
+		self.waitProcess(self.hprocess, timeout=15)
+		
+		# check the process status
+		self.finalstatus = self.hprocess.running())
+		
+		
 	def validate(self):
-		pass
+		# process running status should have been true to start
+		self.assertTrue(self.initialstatus)
+		
+		# process running status should have been false on completion
+		self.assertFalse(self.finalstatus)
+	
+		# check the sdtout of the process
+		self.assertDiff('counter.out', 'ref_counter.out')
+		
