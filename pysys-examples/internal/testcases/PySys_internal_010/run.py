577229d0889f7ee1a2ad1c77ac4e7b6beb8a16e0
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-08-24 20:27:28 +0000

    Addition of test for text file replacement

--- pysys-examples/internal/testcases/PySys_internal_010/run.py
+++ pysys-examples/internal/testcases/PySys_internal_010/run.py
@@ -1,9 +1,23 @@
+from pysys.utils.filereplace import replace
 from pysys.constants import *
 from pysys.basetest import BaseTest
 
 class PySysTest(BaseTest):
 	def execute(self):
-		pass
+		inputFile = "%s/input.txt" % self.input
+		
+		dict={"BIRD":"moorcock", "GILES":"farmer"}
+		replace(inputFile, "%s/output1.txt" % self.output, dict, marker='$')
 
+		dict={"CEREAL":"grain"}
+		replace(inputFile, "%s/output2.txt" % self.output, dict, marker='%')
+		
+		dict={"$BIRD$":"moorcock", "$GILES$":"farmer", "%CEREAL%":"grain", "replaceme":"night"}
+		replace(inputFile, "%s/output3.txt" % self.output, dict, marker='')
+		
+			
 	def validate(self):
-		pass
+		# validate the correct replacement with the $ marker
+		self.assertDiff("output1.txt", "ref_output1.txt")
+		
+		
