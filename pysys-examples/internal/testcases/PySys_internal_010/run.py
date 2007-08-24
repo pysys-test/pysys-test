8cd952e10a646b87a6625f16098a9d94ec239022
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-08-24 20:29:47 +0000

    Added more verification

--- pysys-examples/internal/testcases/PySys_internal_010/run.py
+++ pysys-examples/internal/testcases/PySys_internal_010/run.py
@@ -20,4 +20,10 @@ class PySysTest(BaseTest):
 		# validate the correct replacement with the $ marker
 		self.assertDiff("output1.txt", "ref_output1.txt")
 		
+		# validate the correct replacement with the % marker
+		self.assertDiff("output2.txt", "ref_output2.txt")
+		
+		# validate the correct replacement with no marker
+		self.assertDiff("output3.txt", "ref_output3.txt")
+		
 		
