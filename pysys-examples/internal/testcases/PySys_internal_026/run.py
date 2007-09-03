f8cd70edc379320c0692bf8582e0cd9b9059444d
Moray Grieve <moraygrieve@users.noreply.github.com>
2007-09-03 21:18:30 +0000

    Updated

--- pysys-examples/internal/testcases/PySys_internal_026/run.py
+++ pysys-examples/internal/testcases/PySys_internal_026/run.py
@@ -2,8 +2,20 @@ from pysys.constants import *
 from pysys.basetest import BaseTest
 
 class PySysTest(BaseTest):
-	def execute(self):
-		pass
 
+	def execute(self):
+		script = "%s/internal/utilities/scripts/counter.py" % self.project.root
+
+		self.hprocess = self.startProcess(command=sys.executable,
+						  arguments = [script, "20", "1"],
+						  environs = os.environ,
+						  workingDir = self.output,
+						  stdout = "%s/timeout.out" % self.output,
+						  stderr = "%s/timeout.err" % self.output,
+						  state=FOREGROUND,
+						  timeout=5)
+			
 	def validate(self):
-		pass
+		if self.getOutcome() == TIMEDOUT:
+			self.log.info("Outcome list contains TIMEDOUT %s" % self.outcome)
+			self.outcome = [PASSED]
