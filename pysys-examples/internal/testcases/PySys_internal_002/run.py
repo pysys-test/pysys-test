d0f51c35b05c173555613deb7dc366a339984537
Moray Grieve <moraygrieve@users.noreply.github.com>
2008-04-12 16:30:15 +0000

    Updated

--- pysys-examples/internal/testcases/PySys_internal_002/run.py
+++ pysys-examples/internal/testcases/PySys_internal_002/run.py
@@ -6,6 +6,9 @@ class PySysTest(BaseTest):
 
 	def execute(self):
 		self.proj = Project(self.input)
-	
+		self.log.info(self.proj.version)
+		self.log.info(self.proj.library)
+		self.log.info(self.proj.lib)
+			
 	def validate(self):
 		pass
