0d885569c70815930b44cf53e3aefd693e5870fa
Moray Grieve <moraygrieve@users.noreply.github.com>
2008-06-08 21:06:28 +0000

    Updated test

--- pysys-examples/internal/testcases/PySys_internal_050/run.py
+++ pysys-examples/internal/testcases/PySys_internal_050/run.py
@@ -6,4 +6,11 @@ class PySysTest(BaseTest):
 		pass
 
 	def validate(self):
-		pass
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Bar')
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Foo', contains=FALSE)
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Foo', contains=FALSE)
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Line is a foo', ignores=['Bar'])
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Line is a not foo', ignores=['Bar'], contains=FALSE)
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Bar foo humbug', includes=['humbug'])
+		self.assertLastGrep('file1.txt', filedir=self.input, expr='Bar humbug', includes=['humbug'], ignores=['foo'])
+		
