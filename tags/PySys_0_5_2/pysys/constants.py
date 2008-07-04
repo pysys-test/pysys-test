246946edf74a50b96793dffc5383e91274db15f1
nobody <nobody@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2008-07-04 13:55:11 +0000

    This commit was manufactured by cvs2svn to create tag 'PySys_0_5_2'.

---
+++ tags/PySys_0_5_2/pysys/constants.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and any associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# The software is provided "as is", without warranty of any
+# kind, express or implied, including but not limited to the
+# warranties of merchantability, fitness for a particular purpose
+# and noninfringement. In no event shall the authors or copyright
+# holders be liable for any claim, damages or other liability,
+# whether in an action of contract, tort or otherwise, arising from,
+# out of or in connection with the software or the use or other
+# dealings in the software
+
+import sys, re, os, socket, logging
+
+from pysys import rootLogger
+
+# constants used in testing
+FALSE=0
+TRUE=1
+BACKGROUND = 10
+FOREGROUND = 11
+PASSED = 20
+INSPECT = 21
+NOTVERIFIED = 22
+FAILED = 23
+TIMEDOUT = 24
+DUMPEDCORE = 25
+BLOCKED = 26
+SKIPPED = 27
+WARN = 30
+INFO = 31
+DEBUG = 32
+
+LOOKUP = {}
+LOOKUP[TRUE] = "TRUE"
+LOOKUP[FALSE] = "FALSE"
+LOOKUP[PASSED] = "PASSED"
+LOOKUP[INSPECT] = "REQUIRES INSPECTION"
+LOOKUP[NOTVERIFIED] = "NOT VERIFIED"
+LOOKUP[FAILED] = "FAILED"
+LOOKUP[TIMEDOUT] = "TIMED OUT"
+LOOKUP[DUMPEDCORE] = "DUMPED CORE"
+LOOKUP[BLOCKED] = "BLOCKED"
+LOOKUP[SKIPPED] = "SKIPPED"
+
+
+# set the precedent for the test outcomes
+PRECEDENT = [SKIPPED, BLOCKED, DUMPEDCORE, TIMEDOUT, FAILED, NOTVERIFIED, INSPECT, PASSED]
+FAILS = [ BLOCKED, DUMPEDCORE, TIMEDOUT, FAILED ]
+
+
+# set the default descriptor filename, input, output and reference directory names
+DEFAULT_PROJECTFILE = '.pysysproject'
+DEFAULT_DESCRIPTOR = ['.pysystest', 'descriptor.xml']  
+DEFAULT_MODULE = 'run'
+DEFAULT_GROUP = ""
+DEFAULT_TESTCLASS = 'PySysTest'
+DEFAULT_INPUT = "Input"
+DEFAULT_OUTPUT = "Output"
+DEFAULT_REFERENCE = "Reference"
+DEFAULT_RUNNER_MODULE = 'pysys.baserunner'
+DEFAULT_RUNNER_CLASS = "BaseRunner"
+DEFAULT_LOGWRITER_MODULE = 'pysys.writer'
+DEFAULT_LOGWRITER_CLASS = "LogFileResultsWriter"
+
+# set the directories to not recursively walk when looking for the descriptors
+OSWALK_IGNORES = [ DEFAULT_INPUT, DEFAULT_OUTPUT, DEFAULT_REFERENCE, 'CVS', '.svn' ]
+
+
+# set the timeout values for specific executables when executing a test
+DEFAULT_TIMEOUT = 600
+TIMEOUTS = {}
+TIMEOUTS['WaitForSocket'] = 60
+TIMEOUTS['WaitForFile'] = 30
+TIMEOUTS['WaitForSignal'] = 60
+TIMEOUTS['ManualTester'] = 1800
+
+
+# set the platform and platform related constants
+HOSTNAME = socket.getfqdn()
+if re.search('win32', sys.platform):
+	PLATFORM='win32'	
+	OSFAMILY='windows'
+	DEVNULL = 'nul'
+	ENVSEPERATOR = ';'
+	WINDIR = os.getenv('windir', 'c:\WINDOWS')
+	PATH = r"%s;%s\system32;%s\System32\Wbem" % (WINDIR, WINDIR, WINDIR)
+	LD_LIBRARY_PATH = ""
+
+elif re.search('sunos', sys.platform):
+	PLATFORM='sunos'
+	OSFAMILY='unix'
+	DEVNULL = '/dev/null'
+	ENVSEPERATOR = ':'
+	PATH = "/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/ccs/bin:/usr/openwin/bin:/opt/SUNWspro/bin"
+	LD_LIBRARY_PATH = "/usr/local/lib" 
+
+elif re.search('linux', sys.platform):
+	PLATFORM='linux'	
+	OSFAMILY='unix'
+	DEVNULL = '/dev/null'
+	ENVSEPERATOR = ':'
+	PATH = "/bin:/usr/bin:/usr/sbin:/usr/local/bin"
+	LD_LIBRARY_PATH = "/usr/lib"
+
+
+# reference to the project instance defining parameters for the 
+# pysys project
+PROJECT = None
+
+
+# load the project specific details
+def loadproject(start):
+	"""Load the pysys project file.
+	
+	The method walks up the directory tree from the supplied path until the 
+	.pysysproject file is found. The location of the project file defines
+	the project root location. The contents of the project file determine 
+	project specific constants as specified by property elements in the 
+	xml project file.
+	
+	To ensure that all loaded modules have a pre-initialised projects 
+	instance, any launching application should first import the loadproject
+	file, and then make a call to it prior to importing all names within the
+	constants module.
+
+	"""
+
+	global PROJECT
+
+	search = start
+	drive, path = os.path.splitdrive(search)
+	while (not search == drive) and (not os.path.exists(os.path.join(search, DEFAULT_PROJECTFILE))): 
+		search, drop = os.path.split(search) 
+		if not drop: search = drive
+	PROJECT = Project(search)
+
+
+class Project:
+	"""Class detailing project specific information for a set or pysys tests.
+	
+	Reads and parses the pysys project file if it exists and translates property element 
+	name/value entries in the project file into data attributes of the class instance. 
+	
+	@ivar root: Full path to the project root, as specified by the first .pysysproject
+				file encountered when walking down the directory tree from the start 
+				directory passed into the launcher (e.g. the working directory)  
+	@type root: string
+	
+	"""
+	
+	def __init__(self, root):
+		self.root = root
+		
+		if os.path.exists(os.path.join(root, DEFAULT_PROJECTFILE)):	
+			# parse the project file
+			from pysys.xml.project import XMLProjectParser
+			parser = XMLProjectParser(os.path.join(root, DEFAULT_PROJECTFILE))
+			
+			# get the properties
+			properties = parser.getProperties()
+			keys = properties.keys()
+			keys.sort()
+			for key in keys: setattr(self, key, properties[key])
+
+			# add to the python path
+			parser.addToPath()
+		
+			# get the runner if specified
+			self.runnerClassname, self.runnerModule = parser.getRunnerDetails()
+			
+			# set the data attributes
+			parser.unlink()	
+	
+	
---
+++ tags/PySys_0_5_2/pysys/utils/smtpserver.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and any associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# The software is provided "as is", without warranty of any
+# kind, express or implied, including but not limited to the
+# warranties of merchantability, fitness for a particular purpose
+# and noninfringement. In no event shall the authors or copyright
+# holders be liable for any claim, damages or other liability,
+# whether in an action of contract, tort or otherwise, arising from,
+# out of or in connection with the software or the use or other
+# dealings in the software.
+ 
+import asyncore, smtpd, threading
+
+from pysys.constants import *
+
+
+class SimpleSMTPServer(smtpd.SMTPServer): 
+	def __init__(self, localaddr, remoteaddr, filename='smtpserver.out', logMails=TRUE):
+		smtpd.SMTPServer.__init__(self, localaddr, remoteaddr)
+		try:
+			self.fp = open(filename, 'w')
+			self.logMails = logMails
+			self.count = 0
+		except:
+			pass
+
+	def process_message(self, peer, mailfrom, rcpttos, data):
+		self.count = self.count + 1
+		if self.fp:
+			self.fp.write("SimpleSMTPServer: Message count = %d\n" % self.count)
+			if self.logMails: self.fp.write(data)
+			self.fp.flush()
+
+
+class SimpleSMTPServerRunner:
+	def __init__(self):
+		self.exit = 0
+
+	def kickoff(self):
+		while not self.exit:
+			asyncore.loop(timeout=1.0, use_poll=False, count=1)
+
+	def start(self):
+		threading.Thread(target=self.kickoff).start()
+
+	def stop(self):
+		self.exit = 1
+		
+
+	
+
