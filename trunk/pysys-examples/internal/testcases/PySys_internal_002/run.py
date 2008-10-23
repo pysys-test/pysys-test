5fb7843ed8b18d3fa6487bb6004bd8755c1b0dd8
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2008-10-23 10:58:04 +0000

    Renamed

--- trunk/pysys-examples/internal/testcases/PySys_internal_002/run.py
+++ trunk/pysys-examples/internal/testcases/PySys_internal_002/run.py
@@ -6,7 +6,7 @@ class PySysTest(BaseTest):
 
 	def execute(self):
 		os.environ["TEST_USER"] = "Felicity Kendal"
-		self.proj = Project(self.input)
+		self.proj = Project(self.input, 'pysysproject.xml')
 		for attr in dir(self.proj):
 			if attr in ['lib', 'library', 'version', 'user']:
 				self.log.info("%s = %s", attr, eval("self.proj.%s" % attr))
