7a0faf189603e30bba2ef3c507cc2a44c5ca9226
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2009-09-17 15:32:23 +0000

    Required for python 2.6

--- trunk/pysys-examples/internal/testcases/PySys_internal_024/run.py
+++ trunk/pysys-examples/internal/testcases/PySys_internal_024/run.py
@@ -11,6 +11,7 @@ class PySysTest(BaseTest):
 		env["PYSYS-TEST"] = "Test variable"
 		env["EMPTY-ENV"] = ""
 		env["INT-ENV"] = "1"
+		env["SYSTEMROOT"] = os.environ["SYSTEMROOT"]
 		
 		self.hprocess = self.startProcess(command=sys.executable,
 						  arguments = [script],
