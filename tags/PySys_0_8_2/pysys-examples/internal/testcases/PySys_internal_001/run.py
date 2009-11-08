31980c60648ffae4c66a6d898c3b7dd1f257308c
nobody <nobody@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2009-11-08 10:08:30 +0000

    This commit was manufactured by cvs2svn to create tag 'PySys_0_8_2'.

---
+++ tags/PySys_0_8_2/pysys-examples/internal/testcases/PySys_internal_001/run.py
@@ -0,0 +1,20 @@
+from pysys.constants import *
+from pysys.basetest import BaseTest
+from pysys.constants import Project 
+
+class PySysTest(BaseTest):
+
+	def execute(self):
+		os.environ["TEST_USER"] = "Felicity Kendal"
+		self.proj = Project(self.input, 'pysysproject.xml')
+	
+	def validate(self):
+		self.assertTrue(self.proj.env_user == "Felicity Kendal")
+		self.assertTrue(self.proj.env_user_prepend == "append-on-front-Felicity Kendal")
+		self.assertTrue(self.proj.env_user_append == "Felicity Kendal-append-on-back")
+		self.assertTrue(self.proj.env_default == "default value")
+		self.assertTrue(self.proj.env_default_none == "")
+		self.assertTrue(self.proj.user_firstname == "Simon")
+		self.assertTrue(self.proj.user_lastname == "Smith")
+		self.assertTrue(self.proj.user_title == "Professor")
+		self.assertTrue(self.proj.user_full == "Professor Simon Smith")
---
+++ tags/PySys_0_8_2/pysys-examples/internal/testcases/PySys_internal_024/run.py
@@ -0,0 +1,29 @@
+from pysys.constants import *
+from pysys.basetest import BaseTest
+from pysys.process.helper import ProcessWrapper
+
+class PySysTest(BaseTest):
+
+	def execute(self):
+		script = "%s/environment.py" % self.input
+		env = {}
+		env["PYSYS-USER"] = "Simon Batty"
+		env["PYSYS-TEST"] = "Test variable"
+		env["EMPTY-ENV"] = ""
+		env["INT-ENV"] = "1"
+		env["SYSTEMROOT"] = os.environ["SYSTEMROOT"]
+		
+		self.hprocess = self.startProcess(command=sys.executable,
+						  arguments = [script],
+						  environs = env,
+						  workingDir = self.output,
+						  stdout = "%s/environment.out" % self.output,
+						  stderr = "%s/environment.err" % self.output,
+						  state=FOREGROUND)
+
+		# wait for the strings to be writen to sdtout
+		self.waitForSignal("environment.out", expr="Written process environment", timeout=5)
+			
+	def validate(self):
+		# validate against the reference file
+		self.assertDiff("environment.out", "ref_environment.out")
