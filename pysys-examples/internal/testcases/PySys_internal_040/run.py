8df5dbdbc53148579a1e960b467c5e9ee74a60c1
Moray Grieve <moraygrieve@users.noreply.github.com>
2010-10-25 13:48:45 +0000

    Updated

--- pysys-examples/internal/testcases/PySys_internal_040/run.py
+++ pysys-examples/internal/testcases/PySys_internal_040/run.py
@@ -6,4 +6,25 @@ class PySysTest(BaseTest):
 		pass
 
 	def validate(self):
-		pass
+		self.assertTrue(eval('1==1'))
+		self.assertTrue(eval('12>=5'))
+		self.assertTrue(True)
+		self.assertTrue(eval('True'))
+		self.assertTrue(eval('12*5>30'))
+		
+		self.assertTrue(eval('1!=1'))
+		self.checkForFailedOutcome()
+		
+		self.assertTrue(eval('False'))
+		self.checkForFailedOutcome()
+		
+		self.assertTrue(False)
+		self.checkForFailedOutcome()
+	
+		
+	def checkForFailedOutcome(self):
+		outcome = self.outcome.pop()
+		if outcome == FAILED: self.addOutcome(PASSED)
+		else: self.addOutcome(FAILED)
+		
+		
--- pysys-examples/internal/testcases/PySys_internal_042/run.py
+++ pysys-examples/internal/testcases/PySys_internal_042/run.py
@@ -6,4 +6,25 @@ class PySysTest(BaseTest):
 		pass
 
 	def validate(self):
-		pass
+		self.assertFalse(eval('1==2'))
+		self.assertFalse(eval('12<=5'))
+		self.assertFalse(False)
+		self.assertFalse(eval('False'))
+		self.assertFalse(eval('12*5<30'))
+		
+		self.assertFalse(eval('1==1'))
+		self.checkForFailedOutcome()
+		
+		self.assertFalse(eval('True'))
+		self.checkForFailedOutcome()
+		
+		self.assertFalse(True)
+		self.checkForFailedOutcome()
+	
+		
+	def checkForFailedOutcome(self):
+		outcome = self.outcome.pop()
+		if outcome == FAILED: self.addOutcome(PASSED)
+		else: self.addOutcome(FAILED)
+		
+		
