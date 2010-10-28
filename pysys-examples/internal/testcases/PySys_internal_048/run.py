76fda80582b359ce502f7fdcb68c5596dabbfae8
Moray Grieve <moraygrieve@users.noreply.github.com>
2010-10-28 08:35:14 +0000

    Added in the tests

--- pysys-examples/internal/testcases/PySys_internal_048/run.py
+++ pysys-examples/internal/testcases/PySys_internal_048/run.py
@@ -6,4 +6,30 @@ class PySysTest(BaseTest):
 		pass
 
 	def validate(self):
-		pass
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=['moon shines bright'])
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=['moon shines right'])
+		self.checkForFailedOutcome()
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=['moon shines right'], contains=False)
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=['(?P<tag>moon) shines bright'])
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=['moon.*bright'])
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=['Now eastlin|westlin winds'])
+		
+		exprList=[]
+		exprList.append('(?P<direction>westlin|eastlin)')
+		exprList.append('moon shines bright')
+		exprList.append('my charmer')			
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=exprList)
+		
+		exprList=[]
+		exprList.append('(?P<direction>northlin|eastlin)')
+		exprList.append('moon shines bright')
+		exprList.append('my charmer')			
+		self.assertOrderedGrep(file='file.txt', filedir=self.input, exprList=exprList)
+		self.checkForFailedOutcome()
+		
+		
+	def checkForFailedOutcome(self):
+		outcome = self.outcome.pop()
+		if outcome == FAILED: self.addOutcome(PASSED)
+		else: self.addOutcome(FAILED)
+		
