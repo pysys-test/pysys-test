977d54efe7cdb37e8b4b065c998e1ba89606db20
Moray Grieve <moraygrieve@users.noreply.github.com>
2012-09-26 19:04:08 +0000

    More changes to support migration to Python 3

--- pysys-examples/internal/testcases/PySys_internal_025/Input/workingdir.py
+++ pysys-examples/internal/testcases/PySys_internal_025/Input/workingdir.py
@@ -4,7 +4,7 @@ import os, os.path, sys, string
 
 # entry point for running the script as an executable
 if __name__ == "__main__":
-	sys.stderr.write("Current working directory is %s\n" % string.replace(os.getcwd(), "\\", "/"))
+	sys.stderr.write("Current working directory is %s\n" % os.getcwd().replace("\\", "/"))
 	sys.stderr.flush()
 	
 	sys.stdout.write("Writing contents of working directory\n")
