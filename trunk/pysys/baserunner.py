188225efc290782444f99057c7e166f9561a6225
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2012-10-03 08:03:56 +0000

    Licence text

--- trunk/pysys/baserunner.py
+++ trunk/pysys/baserunner.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-# Permission is hereby granted, free of charge, to any person
-# obtaining a copy of this software and any associated documentation
-# files (the "Software"), to deal in the Software without
-# restriction, including without limitation the rights to use, copy,
-# modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# The software is provided "as is", without warranty of any
-# kind, express or implied, including but not limited to the
-# warranties of merchantability, fitness for a particular purpose
-# and noninfringement. In no event shall the authors or copyright
-# holders be liable for any claim, damages or other liability,
-# whether in an action of contract, tort or otherwise, arising from,
-# out of or in connection with the software or the use or other
-# dealings in the software
+# PySys System Test Framework, Copyright (C) 2006-2012  M.B.Grieve
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Contact: moraygrieve@users.sourceforge.net
 """
 Contains the base class used to perform the execution and audit trail reporting of a set of tests. 
 
