c7e9f4c3d421b51c14afda7ab8dcf7b9ff7df2b4
Moray Grieve <moraygrieve@users.noreply.github.com>
2012-11-03 09:23:36 +0000

    Sorted() isn't inplace

--- pysys/launcher/__init__.py
+++ pysys/launcher/__init__.py
@@ -80,7 +80,7 @@ def createDescriptors(testIdSpecs, type, includes, excludes, trace, dir=None):
 		except Exception, value:
 			print sys.exc_info()[0], sys.exc_info()[1]
 			log.info("Error reading descriptorfile %s" % descriptorfile)
-	sorted(descriptors, key=lambda x: x.file)
+	descriptors = sorted(descriptors, key=lambda x: x.file)
 
 	# trim down the list for those tests in the test specifiers 
 	tests = []
