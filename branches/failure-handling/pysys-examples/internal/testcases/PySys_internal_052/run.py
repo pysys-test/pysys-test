e8cc07dda8bc3d2211b18ff1b0ee8d7d9273a987
ben-spiller <ben-spiller@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2015-02-05 17:17:48 +0000

    create new branch for improving error and test failure handling in various ways

---
+++ branches/failure-handling/pysys-examples/internal/testcases/PySys_internal_052/run.py
@@ -0,0 +1,18 @@
+from pysys.constants import *
+from pysys.utils.filecopy import filecopy
+from pysys.basetest import BaseTest
+
+class PySysTest(BaseTest):
+	def execute(self):
+		self.assertLineCount('file1.txt', filedir=self.input, expr='Foo', condition='==1')
+		self.assertLineCount('file1.txt', filedir=self.input, expr='Fi', condition='>=15')
+		self.waitForSignal('run.log', expr='WARN  Line count on input file file1.txt ... failed', condition='>=1')
+		
+		self.log.info('Copying run.log for later verification')
+		filecopy(os.path.join(self.output, 'run.log'), os.path.join(self.output, 'run.log.proc'))
+		
+	def validate(self):
+		del self.outcome[:]
+		self.assertGrep('run.log.proc', expr='Line count on input file file1.txt ... passed')
+		self.assertGrep('run.log.proc', expr='Line count on input file file1.txt ... failed \[1>=15\]')
+		
