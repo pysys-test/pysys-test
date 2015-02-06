efc4ef5c4554d1a9ebad17a0344caf3d8fdacd8b
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2015-02-06 21:59:38 +0000

    Update copyright on branch before we merge it down

--- branches/failure-handling/pysys/xml/descriptor.py
+++ branches/failure-handling/pysys/xml/descriptor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pytho
-# PySys System Test Framework, Copyright (C) 2006-2013  M.B.Grieve
+# PySys System Test Framework, Copyright (C) 2006-2015  M.B.Grieve
 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
