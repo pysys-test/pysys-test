d6a932d2bf1578dc34e69482225ff43b2f8a6d53
Moray Grieve <moraygrieve@users.noreply.github.com>
2016-01-04 15:38:57 +0000

    For failures, print a call stack also

--- pysys/basetest.py
+++ pysys/basetest.py
@@ -22,7 +22,7 @@ Contains the base test class for test execution and validation.
 For more information see the L{pysys.basetest.BaseTest} API documentation. 
 
 """
-import sys, os, os.path, re, string, time, thread, logging, copy, math, stat
+import sys, os, os.path, re, string, time, thread, logging, copy, math, stat, inspect
 
 from pysys import log
 from pysys.constants import *
@@ -598,3 +598,23 @@ class BaseTest(ProcessUser):
 		"""
 		if xargs.has_key('abortOnError'): return xargs['abortOnError']
 		return PROJECT.defaultAbortOnError.lower()=='true' if hasattr(PROJECT, 'defaultAbortOnError') else DEFAULT_ABORT_ON_ERROR
+
+
+	def addOutcome(self, outcome, outcomeReason='', printReason=True, abortOnError=None):
+		if outcome in FAILS:
+			ProcessUser.addOutcome(self, outcome, outcomeReason, printReason, abortOnError, self.__callRecord())
+		else:
+			ProcessUser.addOutcome(self, outcome, outcomeReason, printReason, abortOnError)
+
+
+	def __callRecord(self):
+		"""Retrieve a call record outside of this module, up to the execute or validate method of the test case.
+
+		"""
+		stack=[]
+		for record in inspect.stack():
+			info = inspect.getframeinfo(record[0])
+			if (info.filename == inspect.getfile(inspect.currentframe()) ): continue
+			stack.append( '%s:%s' % (os.path.basename(info.filename).strip(), info.lineno) )
+			if (info.filename == self.descriptor.module+'.py' and (info.function == 'execute' or info.function == 'validate')): break
+		return stack
