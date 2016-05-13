a174c4bc92b26bf9118a31815ad56bf699f35a37
Ben Spiller <ben-spiller@users.noreply.github.com>
2016-05-13 17:26:50 +0000

    fix assertLineCount failure message which was hard to debug as it didn't specify what the actual value was

--- pysys-examples/internal/testcases/PySys_internal_052/run.py
+++ pysys-examples/internal/testcases/PySys_internal_052/run.py
@@ -6,7 +6,7 @@ class PySysTest(BaseTest):
 	def execute(self):
 		self.assertLineCount('file1.txt', filedir=self.input, expr='Foo', condition='==1')
 		self.assertLineCount('file1.txt', filedir=self.input, expr='Fi', condition='>=15')
-		self.waitForSignal('run.log', expr='Line count on file1.txt for "Fi">=15 ... failed', timeout=5)
+		self.waitForSignal('run.log', expr='Line count .*Fi.*>=15', timeout=5)
 		
 		self.log.info('Copying run.log for later verification')
 		filecopy(os.path.join(self.output, 'run.log'), os.path.join(self.output, 'run.log.proc'))
@@ -14,4 +14,4 @@ class PySysTest(BaseTest):
 	def validate(self):
 		del self.outcome[:]
 		self.assertGrep('run.log.proc', expr='Line count on input file file1.txt ... passed')
-		self.assertGrep('run.log.proc', expr="Line count on file1.txt for \"Fi\">=15 ... failed")
+		self.assertGrep('run.log.proc', expr=" Line count on file1.txt for \"Fi\">=15 [(]actual =1[)] ... failed")
