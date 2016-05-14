7a61a8ee5a0d15245be449f9dc5e1d011c91f843
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2016-05-14 16:51:53 +0000

    Update epydoc

--- trunk/pysys/writer/__init__.py
+++ trunk/pysys/writer/__init__.py
@@ -20,15 +20,15 @@
 """
 Contains implementations of test output summary writers used to output test results during runtime execution. 
 
-There are currently three implementations of writers distributed with the PySys framework, 
-namely the L{writer.TextResultsWriter}, the L{writer.XMLResultsWriter} and the 
-L{writer.JUnitXMLResultsWriter}. Project configuration of the writers is through the PySys 
-project file using the <writer> tag - multiple writers may be configured and their individual 
-properties set through the nested <property> tag. Writer properties are set as attributes to 
-the class through the setattr() function. Custom (site specific) modules can be created and 
-configured by users of the PySys framework (e.g. to output test results into a relational 
-database etc), though they must adhere to the interface demonstrated by the implementations 
-demonstrated here. 
+There are currently four implementations of writers distributed with the PySys framework,
+namely the L{writer.TextResultsWriter}, the L{writer.XMLResultsWriter}, the
+L{writer.JUnitXMLResultsWriter} and the L{writer.CSVResultsWriter}. Project configuration of
+the writers is through the PySys project file using the <writer> tag - multiple writers may
+be configured and their individual properties set through the nested <property> tag. Writer
+properties are set as attributes to the class through the setattr() function. Custom (site
+specific) modules can be created and configured by users of the PySys framework (e.g. to
+output test results into a relational database etc), though they must adhere to the interface
+demonstrated by the implementations demonstrated here.
 
 The writers are instantiated and invoked by the L{pysys.baserunner.BaseRunner} class
 instance. This calls the class constructors of all configured test writers, and then 
