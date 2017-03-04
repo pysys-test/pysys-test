ab2938ec8c2073561a5a1cf56545abb915189a04
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2017-03-04 12:52:44 +0000

    Correct package

--- branches/example_project/rpn-project/src/main/java/org/pysys/examples/rpn/Application.java
+++ branches/example_project/rpn-project/src/main/java/org/pysys/examples/rpn/Application.java
@@ -3,7 +3,7 @@ package org.pysys.examples.rpn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.pysys.examples.rest"})
+@SpringBootApplication(scanBasePackages = {"org.pysys.examples.rpn"})
 public class Application {
 
     public static void main(String[] args) {
