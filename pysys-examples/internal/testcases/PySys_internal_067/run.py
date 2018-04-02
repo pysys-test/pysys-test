a29866bc04f15ba7bf7be39e9c5d110419d799e8
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-04-02 19:12:51 +0000

    add support for higlighting console output with different colors to make it easier to read (implemented in an extensible way to allow customization for different terminal types of use cases), tested by 067

---
+++ pysys-examples/internal/testcases/PySys_internal_067/run.py
@@ -0,0 +1,28 @@
+import pysys
+from pysys.constants import *
+from pysys.basetest import BaseTest
+import os, sys, math, shutil
+
+class PySysTest(BaseTest):
+
+	def execute(self):
+		
+		shutil.copytree(self.input, self.output+'/test')
+
+		env = dict(os.environ)
+		env['PYSYS_COLOR'] = 'tRue'
+		env['PYSYS_TEST_FRIENDLY_ESCAPES'] = 'true'
+		p = self.startProcess(command=sys.executable,
+			arguments = [[a for a in sys.argv if a.endswith('pysys.py')][0], 'run', '-o', self.output+'/myoutdir', '-v', 'DEBUG'],
+			environs = env, workingDir='test',
+			stdout = 'pysys.out', stderr='pysys.err', 
+			ignoreExitStatus=False)
+		self.logFileContents('pysys.out', maxLines=0)
+		#self.assertGrep('pysys.out', expr='Test final outcome: .*(PASSED|NOT VERIFIED)', abortOnError=True)
+			
+	def validate(self):
+		self.assertGrep('pysys.out', expr=r'Failed to format log message', contains=False)
+		self.assertDiff('pysys.out', 'pysys.out', includes=['reason', 'Sample message', 'outcome', 'Id', 'Title', 'FAILED', 'My exception', 'Traceback'])
+		
+		# check run.log not affected
+		self.assertGrep('myoutdir/PySys_NestedTestcase/run.log', expr=r'INFO +skipped reason message ... skipped')
