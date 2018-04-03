950503ad3788e3bd81767e09275fc0e4759e7a96
ben-spiller <ben-spiller@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2018-04-03 14:46:59 +0000

    fix cycle formatting after recent color commit

--- trunk/pysys/baserunner.py
+++ trunk/pysys/baserunner.py
@@ -486,7 +486,7 @@ class TestContainer:
 			title = textwrap.wrap(self.descriptor.title.replace('\n','').strip(), 56)
 			log.info("Id   : %s", self.descriptor.id, extra={DefaultPySysLoggingFormatter.KEY_COLOR_CATEGORY:'details', DefaultPySysLoggingFormatter.KEY_COLOR_ARG_INDEX:0})
 			if self.runner.cycle > 1: 
-				log.info("Cycle: %d", str(self.cycle+1), extra={DefaultPySysLoggingFormatter.KEY_COLOR_CATEGORY:'details', DefaultPySysLoggingFormatter.KEY_COLOR_ARG_INDEX:0})
+				log.info("Cycle: %s", str(self.cycle+1), extra={DefaultPySysLoggingFormatter.KEY_COLOR_CATEGORY:'details', DefaultPySysLoggingFormatter.KEY_COLOR_ARG_INDEX:0})
 			if len(title)>0: log.info("Title: %s", str(title[0]), extra={DefaultPySysLoggingFormatter.KEY_COLOR_CATEGORY:'details', DefaultPySysLoggingFormatter.KEY_COLOR_ARG_INDEX:0})
 			for l in title[1:]: log.info("       %s", l)
 			log.info(62*"=")
