09736b9c0d2a574b4848bd3dd4212d624ac50e43
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-05-04 17:44:14 +0000

    add standard pypi metadata as described on https://packaging.python.org/tutorials/distributing-packages and https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty - first pass, ready for review by Moray

--- pysys-dist/setup.py
+++ pysys-dist/setup.py
@@ -17,7 +17,8 @@
 
 # Contact: moraygrieve@users.sourceforge.net
 
-import pysys, glob, sys, os
+import codecs, os
+import pysys, glob, sys
 import distutils.sysconfig
 from distutils.core import setup
 
@@ -42,14 +43,49 @@ data_files = [(get_site_packages_path(), ['pysys-dist/pysys-release.txt']),
 					  (get_site_packages_path(), ['pysys-dist/README.rst']),
 					  ]
 if IS_WINDOWS: data_files.append(('%s/pysys-doc' % get_site_packages_path(), glob.glob('pysys-doc/*.*')))
-	
+
+# classifiers come from PyPi's official list https://pypi.org/classifiers/
+CLASSIFIERS = [
+	"License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: Developers",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 2",
+	"Programming Language :: Python :: 2.7",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.5",
+	"Programming Language :: Python :: 3.6", # see also python_requires
+	"Programming Language :: Python :: Implementation :: CPython",
+	"Environment :: Console",
+	"Operating System :: Microsoft :: Windows",
+	"Operating System :: POSIX :: SunOS/Solaris",
+	"Operating System :: POSIX :: Linux",
+	"Topic :: Software Development :: Quality Assurance",
+	"Topic :: Software Development :: Testing",
+	"Topic :: Education :: Testing",
+	"Intended Audience :: Developers",
+	"Intended Audience :: Education",
+	"Natural Language :: English",
+]
+KEYWORDS = ['testing', 'qa', 'system testing', 'integration testing', 'unit testing']
+
+with codecs.open(os.path.abspath(os.path.dirname(__file__))+'/README.rst', "rb", "ascii") as f:
+	long_description = f.read()
+
 setup(cmdclass = {'build_py': build_py},
 	  name='PySys',
-	  version=pysys.__version__,
-	  author=pysys.__author__,
-	  author_email=pysys.__author_email__,
 	  description='Python System Test Framework',
 	  url="http://www.sourceforge.net/projects/pysys",
+	  version=pysys.__version__,
+	  author=pysys.__author__,
+	  maintainer=pysys.__author__,
+	  license=pysys.__license__,
+	  author_email=pysys.__author_email__,
+	  keywords=KEYWORDS,
+	  classifiers=CLASSIFIERS,
+	  long_description=long_description,
+	  long_description_content_type='text/x-rst',
+	  python_requires=">=2.7, <4", # be flexible
 	  scripts = ['pysys-dist/scripts/pysys.py', 'pysys-dist/scripts/pysys_postinstall.py'] if IS_WINDOWS
 	  	else ['pysys-dist/scripts/pysys.py'],
 	  packages=['pysys', 'pysys.launcher',  'pysys.manual',
