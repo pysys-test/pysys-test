9d89184c1b1b66d693230674c2518c9f8bd47654
Moray Grieve <moraygrieve@users.noreply.github.com>
2018-05-05 06:32:34 +0000

    Add missing pysys.__license__ and get the build working (the build needs to be updated, but at the moment you run ant deploy.sdist from the pysys-dist directory to create a source bundle distribution)

--- pysys/__init__.py
+++ pysys/__init__.py
@@ -59,6 +59,23 @@ __version__ = "1.3.0"
 __date__ = "06-April-2018"
 """The date of this release."""
 
+__license__ == """
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+"""
+"""The Pysys license."""
+
 __all__     = [ "constants",
                 "exceptions",
                 "baserunner",
