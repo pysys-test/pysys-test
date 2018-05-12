f991bc01878604e6499b8ebf155082397239962f
moraygrieve <moraygrieve@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2018-05-12 09:11:20 +0000

    Blanket update of the year in the copyright

--- trunk/pysys/launcher/console.py
+++ trunk/pysys/launcher/console.py
@@ -1,4 +1,4 @@
-# PySys System Test Framework, Copyright (C) 2006-2016  M.B.Grieve
+# PySys System Test Framework, Copyright (C) 2006-2018  M.B.Grieve
 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
