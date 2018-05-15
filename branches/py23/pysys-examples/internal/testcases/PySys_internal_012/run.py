3804ca4636a7a409793485b3710e1ae120bc8f7a
ben-spiller <ben-spiller@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2018-05-15 17:30:11 +0000

    delete py2+3 branch as it would need rebasing to avoid conflicts with trunk commits

--- branches/py23/pysys-examples/internal/testcases/PySys_internal_012/run.py
+++
@@ -1,9 +0,0 @@
-from pysys.constants import *
-from pysys.basetest import BaseTest
-
-class PySysTest(BaseTest):
-	def execute(self):
-		pass
-
-	def validate(self):
-		self.assertLineCount(file='file1.txt', filedir=self.input, expr='cat', condition='==4')
--- branches/py23/pysys-examples/pyunit/utilities/factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-	if n < 0:
-		raise ArithmeticError('Factorial of negative number')
-	elif n <= 1:
-		return 1
-	else:
-		return factorial(n - 1) * n
-
