19b46d2e0f552019d936f905f4e96c6c50d1f2c9
ben-spiller <ben-spiller@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2018-05-31 10:47:29 +0000

    remove another unused thread import

--- branches/py23/pysys/baserunner.py
+++ branches/py23/pysys/baserunner.py
@@ -26,7 +26,7 @@ list to perform the test execution. For more information see the L{pysys.baserun
 API documentation. 
 
 """
-import os.path, stat, math, logging, textwrap, StringIO, thread
+import os.path, stat, math, logging, textwrap, StringIO
 
 from pysys import ThreadedFileHandler, ThreadedStreamHandler
 from pysys.constants import *
