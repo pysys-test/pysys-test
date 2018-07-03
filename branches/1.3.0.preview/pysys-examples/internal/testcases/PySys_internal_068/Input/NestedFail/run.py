38d367d1c77c716e960fcfc1b26b2d959a734db3
ben-spiller <ben-spiller@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2018-07-03 09:23:56 +0000

    delete now all changes are on trunk

--- branches/1.3.0.preview/pysys-examples/internal/testcases/PySys_internal_068/Input/NestedFail/run.py
+++
@@ -1,9 +0,0 @@
-from pysys.constants import *
-from pysys.basetest import BaseTest
-from pysys.exceptions import *
-
-class PySysTest(BaseTest):
-	def execute(self):
-		self.addOutcome(FAILED) # try one with no outcome reason
-	def validate(self):
-		pass 
--- branches/1.3.0.preview/pysys-examples/internal/testcases/PySys_internal_069/Input/NestedPass/run.py
+++
@@ -1,9 +0,0 @@
-from pysys.constants import *
-from pysys.basetest import BaseTest
-from pysys.exceptions import *
-
-class PySysTest(BaseTest):
-	def execute(self):
-		self.addOutcome(PASSED)
-	def validate(self):
-		pass 
