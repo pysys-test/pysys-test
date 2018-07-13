6ef49fdf2dc4e0d037a69c4f5051fc56de27f2c7
ben-spiller <ben-spiller@13efac9b-802f-43ab-9b7a-fcb15c5ba889>
2018-07-13 13:42:32 +0000

    delete 1.3.0 release tag as not used yet; will be replaced with final 1.3.0 tag with date 13th July 2018

--- tags/PySys_1_3_0/pysys-examples/internal/testcases/PySys_internal_055/Input/wait.py
+++
@@ -1,5 +0,0 @@
-import time, sys
-
-sys.stdout.write("I'm feeling sleepy, very, very, sleepy ...")
-sys.stdout.flush()
-time.sleep(20)
--- tags/PySys_1_3_0/pysys-examples/internal/testcases/PySys_internal_100/Input/deleteFiles.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-
-NUM_FILES = sys.argv[1]
-sys.stdout.write("Deleting %s files...\n" % NUM_FILES)
-for i in range(1, int(NUM_FILES)+1):
-    os.remove(os.path.join(sys.argv[2], "%d.txt" % i))
-sys.stdout.write("Done\n")
