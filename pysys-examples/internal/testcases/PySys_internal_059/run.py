f2eb184596c179dfd9c83a4b5933fb210e0a3ee6
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-10-17 23:55:46 +0000

    fix PySys_internal_059 after stricter checking to avoid expr=None in r1039 (think the test probably never did what it was intended to until now)

--- pysys-examples/internal/testcases/PySys_internal_059/run.py
+++ pysys-examples/internal/testcases/PySys_internal_059/run.py
@@ -14,11 +14,11 @@ class PySysTest(BaseTest):
 						  				stderr = test.stderr,
 						  				state=BACKGROUND, abortOnError=False)
 		try:
-			self.waitForSignal(test.stdout, 'foo', process=p, abortOnError=True)
+			self.waitForSignal(test.stdout, expr='foo', process=p, abortOnError=True)
 			self.addOutcome(FAILED, 'Expected abort')
 		except AbortExecution as e:
 			self.assertThat('%s == %s', e.outcome, BLOCKED)
-			self.assertThat('"due to process '+os.path.basename(sys.executable)+' termination" in "%s"', e.value)
+			self.assertThat('"due to process '+os.path.basename(sys.executable)+' termination" in %s', repr(e.value))
 
 	def validate(self):
 		pass
