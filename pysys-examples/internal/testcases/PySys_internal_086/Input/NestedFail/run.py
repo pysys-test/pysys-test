1803a78ef8dafd38c37ec68933c9f1aedfe235f5
ben-spiller <ben-spiller@users.noreply.github.com>
2019-03-07 18:40:28 +0000

    remove temp debugging line

--- pysys-examples/internal/testcases/PySys_internal_086/Input/NestedFail/run.py
+++ pysys-examples/internal/testcases/PySys_internal_086/Input/NestedFail/run.py
@@ -6,7 +6,6 @@ from pysys.utils.pycompat import *
 
 class PySysTest(BaseTest):
 	def execute(self):
-		#self.log.info('BENBENBEN')
 		self.utf8teststring = b'utf8_European\\xe1\\xc1x\\xdf_Katakana\\uff89\\uff81\\uff90\\uff81\\uff7f\\uff78\\uff81\\uff7d\\uff81\\uff7f\\uff76\\uff72\\uff7d\\uff84_Hiragana\\u65e5\\u672c\\u8a9e_Symbols\\u2620\\u2622\\u2603_abc123@#\\xa3!~=\\xa3x'.decode('unicode_escape')
 		#self.log.info('XXXXXXX %s'%self.utf8teststring.encode('utf-8'))
 
