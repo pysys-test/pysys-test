0591dd78e6843efd52a4a8d95c21dcaf8178878b
ben-spiller <ben-spiller@users.noreply.github.com>
2019-03-18 23:36:02 +0000

    fix tets 97

--- pysys-examples/internal/testcases/PySys_internal_097/Input/PySys_NestedTestcase/Input/test.py
+++ pysys-examples/internal/testcases/PySys_internal_097/Input/PySys_NestedTestcase/Input/test.py
@@ -1,4 +1,4 @@
 import tempfile, os
 print('TempDir=%s'%os.path.normpath(tempfile.gettempdir()))
-print('Python environment: %s.'%', '.join(os.environ.keys()))
+print('Python environment: %s.'%', '.join([k for k in os.environ.keys() if not k.startswith('LC_')]))
 print('Python executed successfully')
