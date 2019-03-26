789884b47877dee5d2f511360e024aa8379c5196
ben-spiller <ben-spiller@users.noreply.github.com>
2019-03-26 16:05:26 +0000

    improve test to spin on a background thread, to check cpu is rolled up from bgthreads

--- pysys-examples/internal/testcases/PySys_internal_080/Input/spinner.py
+++ pysys-examples/internal/testcases/PySys_internal_080/Input/spinner.py
@@ -1,4 +1,7 @@
-import time
+import time,threading
 time.sleep(5)
-while True: pass # spin
-
+# spin on a background thread so we notice if monitoring excluded bg threads
+def bgthread():
+	while True: pass # spin
+# non daemon thread
+threading.Thread(target=bgthread, name="bg-spinner").start()
