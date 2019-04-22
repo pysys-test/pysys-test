897cad7218af16ac3d62a64d464db1eb09dbc0ba
ben-spiller <ben-spiller@users.noreply.github.com>
2019-04-22 21:23:51 +0100

    add some debugging lines to test 103

--- pysys-examples/internal/testcases/PySys_internal_103/run.py
+++ pysys-examples/internal/testcases/PySys_internal_103/run.py
@@ -5,6 +5,17 @@ from pysys.manual.ui import ManualTester
 class PySysTest(BaseTest):
 	def execute(self):
 		self.pythonDocTest(self.input+'/test.py', disableCoverage=True)
+
+		# temp debugging:
+		import pysys
+		self.pythonDocTest(self.input+'/test.py')
+
+		self.pythonDocTest(self.input+'/test.py', disableCoverage=True, pythonPath=sys.path)
+		
+		self.pythonDocTest(os.path.dirname(pysys.__file__)+'/process/user.py', 
+			pythonPath=sys.path)
+
+		
 		assert self.getOutcome() == FAILED, 'expected to fail'
 		reason = self.getOutcomeReason()
 		self.addOutcome(PASSED, 'Doctest failed as expected', override=True)
