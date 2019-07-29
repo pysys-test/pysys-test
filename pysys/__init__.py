17f84a4001bb9db7b7b21b8db915c3d88c1ed32d
ben-spiller <ben-spiller@users.noreply.github.com>
2019-07-29 14:12:41 +0100

    set final version of 1.5.0

--- pysys/__init__.py
+++ pysys/__init__.py
@@ -58,7 +58,7 @@ __maintainer_email__ = "pysys-dev@googlegroups.com"
 __status__  = "Production"
 """The status of this release."""
 
-__version__ = "1.5.0.dev1"
+__version__ = "1.5.0"
 """The version of this release."""
 
 __date__ = "yyyy-mm-dd"
