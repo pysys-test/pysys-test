376b6f69b7e8b923cf7a76f29e4dde28f86de58c
ben-spiller <ben-spiller@users.noreply.github.com>
2020-04-15 12:14:45 +0100

    hide the manual tester package from HTML doc since it's not referenced or usable from anywhere else and isn't really designed or doc'd for extensibiltiy at present

--- pysys/manual/ui.py
+++ pysys/manual/ui.py
@@ -16,6 +16,10 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 
+"""
+The Manual Test User Interface used by `pysys.basetest.BaseTest.startManualTester`.
+"""
+
 
 import sys
 try:
@@ -30,6 +34,24 @@ from pysys.xml.manual import *
 
 
 class ManualTester(object):  # pragma: no cover
+	"""
+	The Manual Test User Interface used by `pysys.basetest.BaseTest.startManualTester`.
+
+	The Manual Test UI is used to detail test steps to be performed via manual intervention, 
+	i.e. when a series of test steps cannot be automated through the execution of an 
+	external process etc. The UI displays the test steps through loading of an XML file 
+	detailing the required information to be presented to the user, and allows for easy 
+	navigation through the steps, and for passing / failing steps that require explicit 
+	verification. 
+
+	Starting of the Manual Test UI is through L{pysys.basetest.BaseTest.startManualTester}, so 
+	it can be performed alongside automated steps, e.g. to setup the test prior to the 
+	manual steps being performed, or to perform as much automation as possible so as to limit the 
+	time required for manual intervention. This approach utilises the automated regression facilities 
+	of the PySys framework, provides autdit trail logging of manual tests as for automated tests, and 
+	makes it easy at a future date to remove the manual steps when automation is possible i.e. through 
+	scriptable record and playback test tools for user interface testing etc.   
+	"""
 	def __init__(self, owner, filename, logname=None):
 		self.parentContainer = None
 		self.owner = owner
@@ -176,6 +198,9 @@ class ManualTester(object):  # pragma: no cover
 		if self.defect != "": self.owner.log.info("Defect - %s recorded with test failure" % self.defect)
 
 	def start(self):
+		"""
+		Called on a background thread by startManualTester. 
+		"""
 		# this runs on a background thread
 		try:
 			self.build()
@@ -200,6 +225,9 @@ class ManualTester(object):  # pragma: no cover
 			self.parentContainer = None
 
 	def running(self):
+		"""
+		Returns 1 after the manual tester is started, and 0 after it has been requested to stop. 
+		"""
 		return self.isRunning
 
 	def parseInputXML(self, input):
