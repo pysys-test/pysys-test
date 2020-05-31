0520b798b6093901606324f5e4896d1d6fbfdb6c
ben-spiller <ben-spiller@users.noreply.github.com>
2020-05-31 18:40:01 +0100

    fix a couple of tests changed by mistake in previous commit

--- pysys-examples/internal/testcases/PySys_internal_085/run.py
+++ pysys-examples/internal/testcases/PySys_internal_085/run.py
@@ -6,8 +6,8 @@ from pysys.basetest import BaseTest
 import os, sys, math, shutil, glob
 import locale
 
-# contains a non-ascii ï¿½ character that is different in utf-8 vs latin-1
-TEST_STR = u'Hello ï¿½ world' 
+# contains a non-ascii £ character that is different in utf-8 vs latin-1
+TEST_STR = u'Hello £ world' 
 
 if PROJECT.testRootDir+'/internal/utilities/extensions' not in sys.path:
 	sys.path.append(PROJECT.testRootDir+'/internal/utilities/extensions') # only do this in internal testcases; normally sys.path should not be changed from within a PySys test
@@ -16,7 +16,7 @@ from pysysinternalhelpers import *
 class PySysTest(BaseTest):
 
 	def execute(self):
-		
+		self.log.info('Preferred encoding = %s, test string = %s', locale.getpreferredencoding(), TEST_STR)
 		if locale.getpreferredencoding() in ['ANSI_X3.4-1968', 'ascii']: self.skipTest('cannot run in ASCII locale')
 
 		self.copy(self.input, self.output+'/test')
