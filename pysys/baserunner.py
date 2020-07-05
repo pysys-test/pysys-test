0d16ba049450b5d4d7bbfeabd5a3c1d8c0a88e84
ben-spiller <ben-spiller@users.noreply.github.com>
2020-07-05 23:36:53 +0100

    treat pythonCoverageDir='' the same as =None, to be on the safe side

--- pysys/baserunner.py
+++ pysys/baserunner.py
@@ -559,7 +559,7 @@ class BaseRunner(ProcessUser):
 		coverage data from other languages, e.g. Java. 		
 		"""
 		pythonCoverageDir = getattr(self.project, 'pythonCoverageDir', None)
-		if self.getBoolProperty('pythonCoverage') and pythonCoverageDir is not None:
+		if self.getBoolProperty('pythonCoverage') and pythonCoverageDir:
 			pythonCoverageDir = os.path.join(self.project.root, pythonCoverageDir
 				.replace('@OUTDIR@', os.path.basename(self.outsubdir))) # matches collect-test-output logic
 			if not pathexists(pythonCoverageDir):
