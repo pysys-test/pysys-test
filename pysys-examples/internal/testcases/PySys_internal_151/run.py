944938b875f048b5e2b894b6b991dda6c89964d8
ben-spiller <ben-spiller@users.noreply.github.com>
2020-08-28 22:51:47 +0100

    try more 151 debugging

--- pysys-examples/internal/testcases/PySys_internal_151/run.py
+++ pysys-examples/internal/testcases/PySys_internal_151/run.py
@@ -13,6 +13,40 @@ class PySysTest(BaseTest):
 		if PY2: self.skipTest('Samples work on Python 3 only')
 		
 		sampledir = self.project.testRootDir+'/../samples/getting-started'
+
+
+		# Start the server application we're testing (as a background process)
+		# self.project provides access to properties in pysysproject.xml, such as appHome which is the 
+		# location of the application we're testing.
+		if True or not IS_WINDOWS:
+			server1 = self.startProcess(
+				command   = sampledir+'/bin/my_server.%s'%('bat' if IS_WINDOWS else 'sh'), 
+				arguments = ['--port', str(self.getNextAvailableTCPPort())], 
+				environs  = os.environ,#self.createEnvirons(addToExePath=os.path.dirname(PYTHON_EXE)),
+				stdouterr = 'my_server1', displayName = 'my_server1<port %s>', background = True,
+				)
+			server2 = self.startProcess(
+				command   = sampledir+'/bin/my_server.%s'%('bat' if IS_WINDOWS else 'sh'), 
+				arguments = ['--port', str(self.getNextAvailableTCPPort())], 
+				environs  = os.environ,#self.createEnvirons(addToExePath=os.path.dirname(PYTHON_EXE)),
+				stdouterr = 'my_server2', displayName = 'my_server2<port %s>', background = True,
+				)
+			# Wait for the server to start by polling for a grep regular expression. The errorExpr/process 
+			# arguments ensure we abort with a really informative message if the server fails to start.
+			try:
+				self.waitForGrep('my_server1.out', '.', errorExpr=[' (ERROR|FATAL) '], process=server1, timeout=8) 
+				self.waitForGrep('my_server1.out', 'Started MyServer .*on port .*', errorExpr=[' (ERROR|FATAL) '], process=server1, timeout=8) 
+			except Exception as ex:
+				self.log.info('Failed to start 1: %s', ex)
+				pass
+			
+			self.logFileContents('my_server1.out')
+			self.logFileContents('my_server2.out')
+			self.logFileContents('my_server1.err')
+			self.logFileContents('my_server2.err')
+			server1.stop()
+			server2.stop()
+		
 	
 		def pysys(name, args, **kwargs):
 			if args[0] == 'run': args = args+['-o', self.output+'/'+name]
