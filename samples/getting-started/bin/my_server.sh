a169acd2732595de9c3545fcd64098e264c82734
ben-spiller <ben-spiller@users.noreply.github.com>
2020-08-28 23:04:10 +0100

    try mroe debugging in teh sample itself

--- samples/getting-started/bin/my_server.sh
+++ samples/getting-started/bin/my_server.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # Unix entry point for our my_server sample application; assumes python3 is on PATH
-python3 "$(dirname $0)/my_server.py" "$@"
+echo Unix server script started
+python3 --version
+echo Now to run "$(dirname $0)/my_server.py"
+python "$(dirname $0)/my_server.py" "$@"
