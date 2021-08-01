86efe0965d7aa5248b2ba9442f347a6f12cc6d06
ben-spiller <ben-spiller@users.noreply.github.com>
2021-08-01 15:41:18 +0100

    use a consistent message for skipped tests

--- test/_pysys_templates/pysys-in-pysys/pysystest.py
+++ test/_pysys_templates/pysys-in-pysys/pysystest.py
@@ -8,9 +8,9 @@ __pysys_purpose__ = r""" The purpose of this test is ... TODO.
 __pysys_authors__ = "@@USERNAME@@"
 __pysys_created__ = "@@DATE@@"
 
-#__pysys_traceability_ids__ = "Bug123, UserStory-456" 
+#__pysys_traceability_ids__ = "BUG-1234, UserStory-456" 
 #__pysys_groups__           = "myGroup, disableCoverage, performance; inherit=true"
-#__pysys_skipped_reason__   = "Skipped until Bug123 is fixed"
+#__pysys_skipped_reason__   = "Skipped until BUG-1234 is fixed"
 #__pysys_modes__            = """ lambda helper: helper.combineModeDimensions(helper.inheritedModes, [{'mode': 'MyMode'}]) """
 
 import pysys
