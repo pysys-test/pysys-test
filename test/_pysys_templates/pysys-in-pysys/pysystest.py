dd5ceccb1b2a00b60447534011103566a9aebe85
ben-spiller <ben-spiller@users.noreply.github.com>
2021-08-01 22:36:56 +0100

    fix pysys template to use default descriptor

--- test/_pysys_templates/pysys-in-pysys/pysystest.py
+++ test/_pysys_templates/pysys-in-pysys/pysystest.py
@@ -1,17 +1,4 @@
-__pysys_title__   = r""" Category TODO - Test title (short, specific and sortable!) goes here TODO """ 
-                         ###############################################################################
-
-__pysys_purpose__ = r""" The purpose of this test is ... TODO.
-	
-	""" 
-	
-__pysys_authors__ = "@@USERNAME@@"
-__pysys_created__ = "@@DATE@@"
-
-#__pysys_traceability_ids__ = "Bug-1234, UserStory-456" 
-#__pysys_groups__           = "myGroup, disableCoverage, performance; inherit=true"
-#__pysys_skipped_reason__   = "Skipped until Bug-1234 is fixed"
-#__pysys_modes__            = """ lambda helper: helper.combineModeDimensions(helper.inheritedModes, [{'mode': 'MyMode'}]) """
+@@DEFAULT_DESCRIPTOR@@
 
 import pysys
 from pysys.constants import *
