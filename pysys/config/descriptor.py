5c9598b0a3a0c412e5794057ed59ffe2f274ff12
ben-spiller <ben-spiller@users.noreply.github.com>
2021-08-06 23:36:51 +0100

    update copyrights to 2021

--- pysys/config/descriptor.py
+++ pysys/config/descriptor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pytho
-# PySys System Test Framework, Copyright (C) 2006-2020 M.B. Grieve
+# PySys System Test Framework, Copyright (C) 2006-2021 M.B. Grieve
 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
