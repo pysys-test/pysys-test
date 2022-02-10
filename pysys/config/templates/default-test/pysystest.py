6497658eedf54bf5d2c1ecc1d286c4ab3b71529a
ben-spiller <ben-spiller@users.noreply.github.com>
2022-02-10 18:37:32 +0000

    add a second (simpler) example for __modes__ to the default test

--- pysys/config/templates/default-test/pysystest.py
+++ pysys/config/templates/default-test/pysystest.py
@@ -8,6 +8,7 @@ __pysys_created__ = "@@DATE@@"
 
 #__pysys_traceability_ids__ = "Bug-1234, UserStory-456" 
 #__pysys_groups__           = "myGroup, disableCoverage, performance"
+#__pysys_modes__            = lambda helper: helper.inheritedModes+[ {'mode':'MyMode', 'MyModeParam':123}, ]
 #__pysys_modes__            = lambda helper: helper.combineModeDimensions(helper.inheritedModes, helper.makeAllPrimary(
 #		{'MyParameterizedSubtestModeA':{'MyParam':123}, 'MyParameterizedSubtestModeB':{'MyParam':456}, }))
 
