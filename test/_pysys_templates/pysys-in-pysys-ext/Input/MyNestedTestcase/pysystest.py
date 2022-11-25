e3afa410a06eae7a31e7c1740914ff8a0928f5be
ben-spiller <ben-spiller@users.noreply.github.com>
2022-11-25 19:09:34 +0000

    update pysys test templatesto avoid over-complication

---
+++ test/_pysys_templates/pysys-in-pysys-ext/Input/MyNestedTestcase/pysystest.py
@@ -0,0 +1,14 @@
+@@DEFAULT_DESCRIPTOR@@
+
+import os, sys, math, shutil, glob
+
+import pysys.basetest, pysys.mappers
+from pysys.constants import *
+
+class PySysTest(pysys.basetest.BaseTest):
+
+	def execute(self):
+		pass
+		
+	def validate(self):
+		pass
---
+++ test/_pysys_templates/pysys-in-pysys-ext/pysystest.py
@@ -0,0 +1,17 @@
+@@DEFAULT_DESCRIPTOR@@
+
+import os, sys, math, shutil, glob
+
+import pysys.basetest, pysys.mappers
+from pysys.constants import *
+
+class PySysTest(pysys.basetest.BaseTest):
+
+	def execute(self):
+
+		#self.copy(self.input, self.output+'/test')
+		self.pysys.pysys('pysys-run', ['run', '-o', self.output+'/myoutdir'], workingDir=self.input)
+		self.logFileContents('pysys-run.out')
+		
+	def validate(self):
+		pass
