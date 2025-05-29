166c29d5108243feb80c403b9b406070db4fa964
ben-spiller <ben-spiller@users.noreply.github.com>
2025-05-29 19:10:58 +0100

    change license/doc files to use full paths from latest build

--- test/correctness/PySys_internal_099/run.py
+++ test/correctness/PySys_internal_099/run.py
@@ -25,7 +25,8 @@ class PySysTest(BaseTest):
 		if distinfo:
 			#self.log.info('Binary dist info contains: %s', sorted(os.listdir(distinfo[0])))
 			for d in rootdocs:
-				self.assertPathExists(distinfo[0]+'/'+os.path.basename(d))
+				self.assertPathExists(distinfo[0]+'/licenses/'+d)
+				#self.assertPathExists(distinfo[0]+'/'+os.path.basename(d))
 		self.log.info('')
 		if isSourceDist:
 			for d in rootdocs:
