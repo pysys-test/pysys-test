14b2f1ec799b380563e0d0b50eb1e0a3d12f6dcc
ben-spiller <ben-spiller@users.noreply.github.com>
2025-05-30 16:54:33 +0100

    try to exclude pysys_docs

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['pysys_docs'] # just use for the build process
 
 
 # -- Options for HTML output -------------------------------------------------
